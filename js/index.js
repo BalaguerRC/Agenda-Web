7ab08f161a208e0c6adaeb459c585b1a4c9b7d22
BalaguerRC <202010776@itla.edu.do>
2022-02-21 18:15:37 -0400

    second commit

--- js/index.js
+++ js/index.js
@@ -1,16 +1,26 @@
 const formulario = document.getElementById('formulario');
+const nombre= document.getElementById('nombre');
+const apellido= document.getElementById('apellido');
+const telefono= document.getElementById('telefono');
 
 formulario.addEventListener('submit', function(e){
     e.preventDefault();
-            
+     
+    /*no funciona
     var formData= new FormData(this);
     console.log(formData);
     console.log(JSON.stringify(formData));
+    */
 
+    const datanew= {nombre: nombre.value, apellido: apellido.value, telefono: telefono.value};
+
+    //http://www.raydelto.org/agenda.php
     fetch('http://www.raydelto.org/agenda.php', {
         method: 'POST',
-        body: formData
-    }).then(res => res.json()).then(text => console.log(text))
+        //body: formData
+        body: JSON.stringify(datanew)
+    }).then(res => res.json())
+    .then(text => console.log(text))
 })
 
 
